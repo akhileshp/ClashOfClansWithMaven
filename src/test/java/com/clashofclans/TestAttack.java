911f021f95af11bf52169af3f34a9dfdafccbaae
Akhilesh Patil <akhileshp@xpanxion.co.in>
2017-07-29 13:24:47 +0530

    Fixed failed circleci build

--- src/test/java/com/clashofclans/TestAttack.java
+++ src/test/java/com/clashofclans/TestAttack.java
@@ -10,7 +10,7 @@ public class TestAttack {
     @Test(expected = BarbarianKilledException.class)
     public void shouldDefenceWinWithOneBarbarianAndOneCannon(){
 
-        final Cannon cannon = new Cannon(10, 3);
+        final Cannon cannon = new Cannon(10, 390);
         final Barbarian barbarian = new Barbarian(8, 45);
 
         new Game().armyAttackOnVillage(cannon, barbarian);
